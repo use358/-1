6b43217f4092b7f8b1a9b84a88bdaedb171d9a96
xiaxx@yunjiglobal.com <1qaz@WSX!@#>
2020-08-28 21:04:57 +0800

    提交代码

---
+++ plan_main.py
@@ -0,0 +1,21 @@
+import pygame
+from plan_sprites import *
+
+
+class PlanGame(object):
+    """飞机大战主游戏"""
+
+    def __init__(self):
+        print("游戏初始化")
+
+
+    def start_game(self):
+        print("游戏开始...")
+
+if __name__ == '__main__':
+
+    # 创建游戏对象
+    game = PlanGame()
+
+    # 启动游戏
+    game.start_game()
